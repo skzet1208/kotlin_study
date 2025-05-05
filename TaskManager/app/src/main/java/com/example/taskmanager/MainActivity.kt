451f80b9ac9dd1dd98cb6c038c6e5559869607fd
鈴木開 <skzet1208@gmail.com>
2025-05-05 22:12:49 +0900

    add Task Manager.

---
+++ TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaskManagerTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) { innerPadding ->
+                    TaskCompletedApp(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskCompletedApp(modifier: Modifier) {
+    TaskCompleted(
+        title = stringResource(R.string.all_tasks_completed),
+        shortText = stringResource(R.string.nice_work),
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun TaskCompleted(title: String, shortText: String, modifier: Modifier) {
+    Column (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_task_completed),
+            contentDescription = null,
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(top = 24.dp, bottom = 8.dp),
+        )
+        Text(
+            text = shortText,
+            fontSize = 16.sp,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TaskManagerTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            TaskCompletedApp(modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
